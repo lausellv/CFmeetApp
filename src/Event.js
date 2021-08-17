ae2ede697396e7660269040c1d9f107fec069a73
Victor <lausellv@yahoo.com>
2021-08-17 20:43:02 +0200

    added event function handler

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { Component} from "react";
 
-export default class Event extends React.Component {
+class Event extends Component {
+  state = {
+    show: false,
+  };
+
+  handleButton = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
   render() {
-    return <div>Event</div>;
+    const event = this.props.event;
+    return (
+      <ul className="event">
+        <li className="summary">{event.summary}</li>
+        <li className="location">{event.location}</li>
+        <li className="date">{event.start.dateTime}</li>
+        <li className="timeZone">{event.start.timeZone}</li>
+        {this.state.show === true && (
+          <p className="details">{event.description}</p>
+        )}
+        {this.state.show === false && (
+          <button className="showMore" onClick={() => this.handleButton()}>
+            Show more
+          </button>
+        )}
+        {this.state.show === true && (
+          <button className="showLess" onClick={() => this.handleButton()}>
+            Show less
+          </button>
+        )}
+      </ul>
+    );
   }
 }
-
-
+export default Event
