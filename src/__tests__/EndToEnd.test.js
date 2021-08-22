1a3346bd6759c7fb8159f1140e640f6a6ad4e012
Victor <lausellv@yahoo.com>
2021-08-22 13:08:04 +0200

    WIP

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -11,7 +11,7 @@ describe("show/hide and event details", () => {
     //    sloMo: 500,
     //    ignoreDefaultArgs: ["--disable-extensions"]
     // }
-  
+
     page = await browser.newPage();
     await page.goto("http://localhost:3000/");
     await page.waitForSelector(".CitySearch");
@@ -20,19 +20,19 @@ describe("show/hide and event details", () => {
   afterAll(() => {
     browser.close();
   });
-//Scenario 1
+  //Scenario 1
   test("An event element is collapsed by default", async () => {
     const eventDetails = await page.$(".event .details");
     expect(eventDetails).toBeNull();
     // browser.close();
   });
-//Scenario 2
+  //Scenario 2
   test("User can expand an event to see its details", async () => {
     await page.click(".event .showMore");
     const eventDetails = await page.$(".event .details");
     expect(eventDetails).toBeDefined();
   });
-
+//scenario 3
   test("User can collapse an event to hide its details", async () => {
     await page.click(".event .showLess");
     const eventDetails = await page.$(".event .details");
