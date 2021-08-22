df0aad0b2f4de03fa27f24b85742da41e7856150
Victor <lausellv@yahoo.com>
2021-08-22 12:13:59 +0200

    chaged selector to CitySearch

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -5,28 +5,28 @@ describe("show/hide and event details", () => {
   let page;
   beforeAll(async () => {
     jest.setTimeout(30000);
-    browser = await puppeteer.launch(
-      {
-      headless: true,
-       sloMo: 500,
-       ignoreDefaultArgs: ["--disable-extensions"]
-    }
-    );
+    browser = await puppeteer.launch();
+    //   {
+    //   headless: true,
+    //    sloMo: 500,
+    //    ignoreDefaultArgs: ["--disable-extensions"]
+    // }
+  
     page = await browser.newPage();
     await page.goto("http://localhost:3000/");
-    await page.waitForSelector(".event");
+    await page.waitForSelector(".CitySearch");
   });
 
   afterAll(() => {
     browser.close();
   });
-
+//Scenario 1
   test("An event element is collapsed by default", async () => {
     const eventDetails = await page.$(".event .details");
     expect(eventDetails).toBeNull();
     // browser.close();
   });
-
+//Scenario 2
   test("User can expand an event to see its details", async () => {
     await page.click(".event .showMore");
     const eventDetails = await page.$(".event .details");
