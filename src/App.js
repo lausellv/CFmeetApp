ba9ed72abfa1362d7326081d5d6e42b0a0fcf725
Victor <lausellv@yahoo.com>
2021-09-01 22:18:48 +0200

    4.8 final touches

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ export default class App extends Component {
         <h4>Events in each city</h4>
         <div className="data-vis-wrapper">
           <EventGenre events={this.state.events} />
-        <ResponsiveContainer height={400}>
+        <ResponsiveContainer height={400} width='80%' className="recharts-responsive-container">
           <ScatterChart
             margin={{
               top: 20,
--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,21 +2,35 @@ import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from "recharts";
 
 const EventGenre = ({ events }) => {
-  const [data, setData] = useState([]);
+  
   const colors = ["#61DBFB", "#F0DB4F", "#68A063", "#0868AC", "#B52E31)"];
 
-  const getData = () => {
-    const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
-    const data = genres.map(genre => {
-      const value = events.filter(event => event.summary.split(" ").includes(genre)).length;
-      return { name: genre, value };
-    });
-    return data;
-  };
+  // const getData = () => {
+  //   const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
+  //   const data = genres.map(genre => {
+  //     const value = events.filter(event => event.summary.split(" ").includes(genre)).length;
+  //     return { name: genre, value };
+  //   });
+  //   return data;
+  // };
+
+  // useEffect(() => {
+  //   setData(() => getData());
+  // }, [events]);
 
   useEffect(() => {
+    const getData = () => {
+      const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
+      const data = genres.map(genre => {
+        const value = events.filter(event => event.summary.split(" ").includes(genre)).length;
+        return { name: genre, value };
+      });
+      return data;
+    };
     setData(() => getData());
   }, [events]);
+
+  const [data, setData] = useState([]);
 
   return (
     <ResponsiveContainer height={400}>
