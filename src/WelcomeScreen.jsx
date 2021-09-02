8c273013dcd258fec4b916b2763ac7c9d6dd4b67
Victor <lausellv@yahoo.com>
2021-09-02 20:26:26 +0200

    empty commit

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -3,7 +3,7 @@ import "./WelcomeScreen.css";
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
     <div className="WelcomeScreen">
-      <h1>Welcome to the Meet app</h1></br>
+      <h1>Welcome to the Meet app</h1>
       <h4>A place for Developers to network with other Developers!</h4> 
       <h4>Log in to see upcoming events around the world for full-stack developers</h4>
       <div className="button_cont" align="center">
